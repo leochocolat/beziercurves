d00e6c3fbcfe105efb38380ae85bc8d9083668fe
leomouraire1 <leomouraire1@gmail.com>
2019-09-30 11:06:39 +0200

    fix bug

--- dist/javascript/app.bundle.js
+++ dist/javascript/app.bundle.js
@@ -128,7 +128,7 @@ eval("/* WEBPACK VAR INJECTION */(function(global, module) {var __WEBPACK_AMD_DE
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or directory, open 'C:\\\\Users\\\\user\\\\Documents\\\\Sites\\\\beziercurves\\\\node_modules\\\\webpack\\\\buildin\\\\global.js'\");\n\n//# sourceURL=webpack:///(webpack)/buildin/global.js?");
+eval("var g;\n\n// This works in non-strict mode\ng = (function() {\n\treturn this;\n})();\n\ntry {\n\t// This works if eval is allowed (see CSP)\n\tg = g || new Function(\"return this\")();\n} catch (e) {\n\t// This works if the window reference is available\n\tif (typeof window === \"object\") g = window;\n}\n\n// g can still be undefined, but nothing to do about it...\n// We return undefined, instead of nothing here, so it's\n// easier to handle this case. if(!global) { ...}\n\nmodule.exports = g;\n\n\n//# sourceURL=webpack:///(webpack)/buildin/global.js?");
 
 /***/ }),
 
@@ -139,7 +139,7 @@ eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or dire
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or directory, open 'C:\\\\Users\\\\user\\\\Documents\\\\Sites\\\\beziercurves\\\\node_modules\\\\webpack\\\\buildin\\\\harmony-module.js'\");\n\n//# sourceURL=webpack:///(webpack)/buildin/harmony-module.js?");
+eval("module.exports = function(originalModule) {\n\tif (!originalModule.webpackPolyfill) {\n\t\tvar module = Object.create(originalModule);\n\t\t// module.parent = undefined by default\n\t\tif (!module.children) module.children = [];\n\t\tObject.defineProperty(module, \"loaded\", {\n\t\t\tenumerable: true,\n\t\t\tget: function() {\n\t\t\t\treturn module.l;\n\t\t\t}\n\t\t});\n\t\tObject.defineProperty(module, \"id\", {\n\t\t\tenumerable: true,\n\t\t\tget: function() {\n\t\t\t\treturn module.i;\n\t\t\t}\n\t\t});\n\t\tObject.defineProperty(module, \"exports\", {\n\t\t\tenumerable: true\n\t\t});\n\t\tmodule.webpackPolyfill = 1;\n\t}\n\treturn module;\n};\n\n\n//# sourceURL=webpack:///(webpack)/buildin/harmony-module.js?");
 
 /***/ }),
 
@@ -150,7 +150,7 @@ eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or dire
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or directory, open 'C:\\\\Users\\\\user\\\\Documents\\\\Sites\\\\beziercurves\\\\node_modules\\\\webpack\\\\buildin\\\\module.js'\");\n\n//# sourceURL=webpack:///(webpack)/buildin/module.js?");
+eval("module.exports = function(module) {\n\tif (!module.webpackPolyfill) {\n\t\tmodule.deprecate = function() {};\n\t\tmodule.paths = [];\n\t\t// module.parent = undefined by default\n\t\tif (!module.children) module.children = [];\n\t\tObject.defineProperty(module, \"loaded\", {\n\t\t\tenumerable: true,\n\t\t\tget: function() {\n\t\t\t\treturn module.l;\n\t\t\t}\n\t\t});\n\t\tObject.defineProperty(module, \"id\", {\n\t\t\tenumerable: true,\n\t\t\tget: function() {\n\t\t\t\treturn module.i;\n\t\t\t}\n\t\t});\n\t\tmodule.webpackPolyfill = 1;\n\t}\n\treturn module;\n};\n\n\n//# sourceURL=webpack:///(webpack)/buildin/module.js?");
 
 /***/ }),
 
